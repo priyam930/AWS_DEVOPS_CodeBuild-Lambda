125532071a7092703e6f97cd97752865c45cc3c5
priyam930 <priyamsanodiya340@gmail.com>
2024-09-09 23:50:24 +0530

    fourth

--- lambda_function.py
+++ lambda_function.py
@@ -2,8 +2,8 @@ import json
 
 def lambda_handler(event, context):
     # TODO implement
-    print("hello priyam!!!")
+    print("hello priyam!!db!")
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda! form linux world!! ')
+        'body': json.dumps('Hello from Lambda! form linux world!db! ')
     }
